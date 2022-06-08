7cbdb8054d2f97dde89723ebab9d87a965a45116
haozheng <1741167304@qq.com>
2022-06-08 08:05:05 +0800

    note

--- server.js
+++ server.js
@@ -24,12 +24,15 @@ var server = http.createServer(function(request, response){
   if(path === '/'){
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
-    response.write(`二哈`)
+    response.write(`
+    <link rel="stylesheet" href="/x">
+    <h1>你好</h1>
+    `)
     response.end()
   } else if(path === '/x'){
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/css;charset=utf-8')
-    response.write(`body{color: red;}`)
+    response.write(`h1{color: green;}`)
     response.end()
   } else {
     response.statusCode = 404
